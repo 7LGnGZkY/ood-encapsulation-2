c2f391f66e3712d2007488e279457c3e4b2e1a96
7LGnGZkY <n35.nnlpd@slmail.me>
2022-06-17 20:08:18 +0100

    Exercise complete

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,31 +1,30 @@
 class TaskList {
+   constructor() {
+      this.tasks = [];
+   }
 
-  constructor() {
-    this.tasks = []
-  }
+   addTask(task) {
+      this.tasks.push(task);
+   }
 
-  addTask(task) {
-    this.tasks.push(task)
-  }
-
-  getOverdueTasks() {
-    const today = new Date()
-    const overdueTasks = []
-    for (const task of this.tasks) {
-      //it's not been completed
-      if (task.status === "incomplete") {
-        //if it has a due date
-        if (task.dateDue !== null) {
-          //if the due date has passed then the task is overdue
-          if (today > task.dateDue) {
-            overdueTasks.push(task)
-          }
-        }
+   getOverdueTasks() {
+      const today = new Date();
+      const overdueTasks = [];
+      for (const task of this.tasks) {
+         //incomplete
+         if (task.status === 'incomplete') {
+            //if due date
+            if (task.dateDue !== null) {
+               //if due date has passed then the task overdue
+               if (today > task.dateDue) {
+                  overdueTasks.push(task);
+               }
+            }
+         }
       }
-    }
 
-    return overdueTasks
-  }
+      return overdueTasks;
+   }
 }
 
-module.exports = TaskList
+module.exports = TaskList;
